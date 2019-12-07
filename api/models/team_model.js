ed734e715659db7819f6bdd8c170af5913833d91
ddenekew <42589144+ddenekew@users.noreply.github.com>
2019-12-06 19:30:16 -0600

    Updated

--- api/models/team_model.js
+++ api/models/team_model.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const teamSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    club: String,
-    country: String
+    club: { type: String, require: true},
+    country: { type: String, require: true}
 });
 
 module.exports = mongoose.model('Team', teamSchema);
