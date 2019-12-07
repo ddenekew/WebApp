215fcfe51761b472fcdf8643a1a6fd4f34604e97
ddenekew <42589144+ddenekew@users.noreply.github.com>
2019-12-06 20:46:42 -0600

    api/routes

--- api/routes/teams.js
+++ api/routes/teams.js
@@ -1,21 +1,53 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-
 const Team = require('../models/team_model');
+const multer = require('multer');
+
+const myStorage = multer.diskStorage( {
+    destination: (req, file, cb) => {
+        cb(null, './uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+const myFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        // save the image
+        cb(null, true);
+    } else {
+        // reject the image
+        cb(null, false);
+    }
+};
+
+const upload = multer({
+    storage: myStorage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: myFilter
+});
 
 router.get('/', (req, res, next) => {
     Team.find()
         .exec()
         .then(docs => {
-            console.log(docs);
-            // if (docs.length >= 0) {
-                res.status(200).json(docs);
-            // } else {
-            //     res.status(404).json({
-            //         message: 'No entries found'
-            //     });
-            // }
+            const response = {
+                count: docs.length,
+                teams: docs.map(doc => {
+                    return {
+                        team: doc,
+                        request: {
+                            type: "GET",
+                            url: "http://localhost:3000/teams/" + doc._id
+                        }
+                    }
+                })
+            }
+                res.status(200).json(response);
         })
         .catch(err => {
             console.log(err);
@@ -25,19 +57,30 @@ router.get('/', (req, res, next) => {
         });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', upload.single('teamImage'), (req, res, next) => {
+    console.log(req.file);
     const team = new Team({
         _id: new mongoose.Types.ObjectId(),
         club: req.body.club,
-        country: req.body.country
+        country: req.body.country,
+        teamImage: req.file.path
     });
     team
         .save()
         .then(result => {
             console.log(result);
             res.status(201).json({
-                message: 'Handling POST requests to /teams',
-                createdTeam: team
+                message: 'Created team successfully',
+                createdTeam: {
+                    club: result.club,
+                    country: result.country,
+                    _id: result._id,
+                    crazy_lable: result.teamImage
+                },
+                request: {
+                    type: "GET",
+                    url: "http://localhost:3000/teams/" + result._id
+                }
             });
         })
         .catch(err => {
@@ -51,10 +94,17 @@ router.post('/', (req, res, next) => {
 router.get('/:teamID', (req, res, next) => {
     const id = req.params.teamID;
     Team.findById(id)
+        .select('-__v')
         .exec()
         .then(doc => {
-            console.log("From DB:", doc);
-            res.status(200).json(doc)
+            res.status(200).json({
+                team: doc,
+                request: {
+                    type: 'GET',
+                    description: 'Get all teams',
+                    url: 'http://localhost:3000/teams'
+                }
+            });
         })
         .catch(err => {
             console.log(err);
@@ -71,8 +121,13 @@ router.patch('/:teamtID', (req, res, next) => {
     team.update({ _id: id}, {$set: updateOps })
         .exec()
         .then(result => {
-            console.log(result);
-            res.status(200).json(result);
+            res.status(200).json({
+                message: 'Team updated',
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/teams/' + id
+                }
+            });
         })
         .catch(err => {
             console.log(err);
@@ -82,10 +137,17 @@ router.patch('/:teamtID', (req, res, next) => {
 
 router.delete('/:teamID', (req, res, next) => {
     const id = req.params.teamID;
-    Product.remove({ _id: id})
+    team.deleteOne({ _id: id})
         .exec()
         .then(result => {
-            res.status(200).json(result);
+            res.status(200).json({
+                message: 'Team deleted',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:3000/teams',
+                    body: { club: 'String', country: 'String' }
+                }
+            });
         })
         .catch(err => {
             console.log(err);
